1b3597d80fc7dfa2bd2fb32bef635202abe0b702
Your Name <163199516+PomidorOgurets@users.noreply.github.com>
2026-02-01 18:29:27 +0300

    Сделаны все 4 задания.

--- test_piter.py
+++ test_piter.py
@@ -1,7 +1,8 @@
-import time
+import pytest
 from playwright.sync_api import expect
 
-def test_fill_page(page):
+@pytest.mark.parametrize("run", range(5))
+def test_fill_page(page, run):
 
     page.goto("https://piter-online.net/")
     page.get_by_role("combobox", name="Введите улицу").click()
@@ -10,7 +11,7 @@ def test_fill_page(page):
     page.get_by_role("combobox", name="Дом").fill("1")
     page.get_by_role("combobox", name="Дом").press("Enter")
 
-    time.sleep(1) # Надо поменять на что-то
+    page.wait_for_timeout(1000)
     page.get_by_role("button", name="Найти тарифы").wait_for(state="visible", timeout=10000)
     page.get_by_role("button", name="Найти тарифы").click()
 
@@ -25,13 +26,16 @@ def test_fill_page(page):
     expect(page.get_by_role("heading", name="Интернет и ТВ по адресу Ленинградская область, линия Тестовая")).to_be_visible()
 
 
-def test_change_region(page):
+@pytest.mark.parametrize("run", range(5))
+def test_change_region(page, run):
     page.goto("https://piter-online.net/")
     page.get_by_role("banner").get_by_role("link", name="Санкт-Петербург").click()
     page.get_by_role("link", name="Гатчина Ленинградская область").click()
     expect(page.get_by_text("Гатчина", exact=True)).to_be_visible()
 
-def test_status(page):
+
+@pytest.mark.parametrize("run", range(5))
+def test_status(page, run):
     # Переход на главную и проверка HTTP 200
     response = page.goto("https://piter-online.net/")
     assert response.status == 200, f"Главная страница вернула статус {response.status} вместо 200"
