619b422676e8c99c61f8e504f92f9d79bea35253
Your Name <163199516+PomidorOgurets@users.noreply.github.com>
2026-02-01 18:19:35 +0300

    Сделаны 1-3 задания.

--- conftest.py
+++ conftest.py
@@ -1,14 +1,11 @@
 import pytest
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import Playwright, sync_playwright
 
-@pytest.fixture(scope="function", autouse=True)
-def browser_setting():
-    browser = sync_playwright().chromium.launch(headless=False)
+@pytest.fixture(scope="function")
+def page(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://piter-online.net/")
-
-    yield
-
+    yield page
     context.close()
     browser.close()
--- test_piter.py
+++ test_piter.py
@@ -1,35 +1,45 @@
-import re
 import time
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import expect
 
+def test_fill_page(page):
 
-def test_run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
     page.goto("https://piter-online.net/")
     page.get_by_role("combobox", name="Введите улицу").click()
     page.get_by_role("combobox", name="Введите улицу").fill("Тестовая линия")
     page.get_by_text("Тестовая линия").click()
     page.get_by_role("combobox", name="Дом").fill("1")
     page.get_by_role("combobox", name="Дом").press("Enter")
-    time.sleep(2)
+
+    time.sleep(1) # Надо поменять на что-то
+    page.get_by_role("button", name="Найти тарифы").wait_for(state="visible", timeout=10000)
     page.get_by_role("button", name="Найти тарифы").click()
-    page.get_by_role("textbox", name="+7 (").click()
+
     page.get_by_role("textbox", name="+7 (").fill("+7 (111) 111-11-111")
     page.get_by_role("button", name="Показать результаты").click()
-    time.sleep(5)
-    page.get_by_role("button", name="Смотреть тарифы").click()
-    expect(page.get_by_role("heading", name="Интернет и ТВ по адресу Ленинградская область, линия Тестовая")).to_be_visible()
-    time.sleep(10)
-    response = page.goto("https://piter-online.net/")
 
+
+    page.get_by_role("button", name="Смотреть тарифы").wait_for(state="visible", timeout=10000)
+    page.get_by_role("button", name="Смотреть тарифы").click()
+
+
+    expect(page.get_by_role("heading", name="Интернет и ТВ по адресу Ленинградская область, линия Тестовая")).to_be_visible()
+
+
+def test_change_region(page):
+    page.goto("https://piter-online.net/")
+    page.get_by_role("banner").get_by_role("link", name="Санкт-Петербург").click()
+    page.get_by_role("link", name="Гатчина Ленинградская область").click()
+    expect(page.get_by_text("Гатчина", exact=True)).to_be_visible()
+
+def test_status(page):
+    # Переход на главную и проверка HTTP 200
+    response = page.goto("https://piter-online.net/")
     assert response.status == 200, f"Главная страница вернула статус {response.status} вместо 200"
 
-    # ---------------------
-    context.close()
-    browser.close()
 
 
-with sync_playwright() as playwright:
-    test_run(playwright)
+
+
+
+
+
